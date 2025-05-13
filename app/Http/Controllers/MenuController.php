5e291b7b478647f51724c65d01365507e0463523
Bintangputraa <100206734+Bintangputraa@users.noreply.github.com>
2025-05-13 17:40:54 +0700

    Update MenuController.php

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -13,7 +13,7 @@ class MenuController extends Controller
     // Menampilkan semua data menu
     public function index()
     {
-        $menus = menu::all();
+        $menus = Menu::all();
         return view('menu', compact('menus'));
     }
 
