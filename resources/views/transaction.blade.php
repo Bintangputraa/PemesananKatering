a78477b52a8d205011761833f2309c01d8e6429e
Bintangputraa <100206734+Bintangputraa@users.noreply.github.com>
2025-05-14 21:52:21 +0700

    Update transaction.blade.php

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -8,11 +8,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Admin Panel</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/transaction">Pesanan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/menu">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/user">User</a>
+                    </li>
+                </ul>
+                <form action="/logout" method="POST" class="d-flex">
+                    @csrf
+                    <button class="btn btn-outline-light" type="submit">Logout</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
     <div class="container mt-4">
         <h2>Daftar Transaksi</h2>
         <div class="table-responsive">
             <table class="table table-striped table-bordered">
-                <thead>
+                <thead class="table-primary">
                     <tr>
                         <th>ID</th>
                         <th>User ID</th>
